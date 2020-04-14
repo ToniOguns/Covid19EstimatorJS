e2337faf7e91122d84d75dae32e735c228832a4b
Oluwatoni Ogunfusika <oluwatoni.ogunfusika@quomodosystems.com>
2020-04-14 01:05:28 +0100

    Push

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var express = require("express");
 var router = express.Router();
 var index = require("../src/index.js");
 
+
 router.get("/api/v1/on-covid-19", index.welcome);
 router.post("/api/v1/on-covid-19", index.dataJson);
 router.post("/api/v1/on-covid-19/json", index.dataJson);
